8eb916a3a0fd9a533aeba8f9a7f5c24d78cddb8d
Daimona Eaytoy <daimona.wiki@gmail.com>
2019-10-20 11:28:04 +0000

    Unbreak CI
    The code tries to log the value of a global unconditionally, but the if
    below is checking whether the global exists.
    
    Change-Id: Ie8f14cb22b6bdba0261f5e041f8801d803c9d696

--- includes/PluggableAuth.php
+++ includes/PluggableAuth.php
@@ -37,7 +37,10 @@ abstract class PluggableAuth {
 	 */
 	public static function singleton() {
 		wfDebugLog( 'PluggableAuth', 'Getting PluggableAuth singleton' );
-		wfDebugLog( 'PluggableAuth', 'Class name: ' . $GLOBALS['wgPluggableAuth_Class'] );
+		wfDebugLog(
+			'PluggableAuth',
+			'Class name: ' . ( $GLOBALS['wgPluggableAuth_Class'] ?? 'unset' )
+		);
 		if ( !is_null( self::$instance ) ) {
 			wfDebugLog( 'PluggableAuth', 'Singleton already exists' );
 			return self::$instance;
