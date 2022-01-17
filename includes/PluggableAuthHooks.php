c60c53e31f3bc23d191a17fc55e1364aefccab21
cicalese <cindom@gmail.com>
2022-01-16 20:10:37 -0500

    Switch to new hook system
    Change-Id: I12fa70eb2254e923f26c6a1110c96d227bcca79a

--- includes/PluggableAuthHooks.php
+++ includes/PluggableAuthHooks.php
@@ -22,14 +22,132 @@
 namespace MediaWiki\Extension\PluggableAuth;
 
 use MediaWiki;
-use MediaWiki\MediaWikiServices;
+use MediaWiki\Auth\Hook\LocalUserCreatedHook;
+use MediaWiki\Hook\BeforeInitializeHook;
+use MediaWiki\Hook\PersonalUrlsHook;
+use MediaWiki\Hook\UserLogoutCompleteHook;
+use MediaWiki\Permissions\Hook\TitleReadWhitelistHook;
+use MediaWiki\SpecialPage\Hook\AuthChangeFormFieldsHook;
+use MWException;
 use OutputPage;
 use SkinTemplate;
 use Title;
 use User;
 use WebRequest;
 
-class PluggableAuthHooks {
+class PluggableAuthHooks implements
+	TitleReadWhitelistHook,
+	AuthChangeFormFieldsHook,
+	UserLogoutCompleteHook,
+	BeforeInitializeHook,
+	PersonalUrlsHook,
+	LocalUserCreatedHook
+{
+
+	/**
+	 * @var PluggableAuthService
+	 */
+	private $pluggableAuthService;
+
+	/**
+	 * @param PluggableAuthService $pluggableAuthService
+	 */
+	public function __construct( PluggableAuthService $pluggableAuthService ) {
+		$this->pluggableAuthService = $pluggableAuthService;
+	}
+
+	/**
+	 * Implements TitleReadWhitelist hook.
+	 * See https://www.mediawiki.org/wiki/Manual:Hooks/TitleReadWhitelist
+	 * Adds PluggableAuth login special pages to whitelist.
+	 *
+	 * @param Title $title being checked
+	 * @param User $user Current user
+	 * @param bool &$whitelisted whether this title is whitelisted
+	 * @since 2.0
+	 */
+	public function onTitleReadWhitelist( $title, $user, &$whitelisted ) {
+		$this->pluggableAuthService->allowLoginPage( $title, $whitelisted );
+	}
+
+	/**
+	 * Implements AuthChangeFormFields hook.
+	 * See https://www.mediawiki.org/wiki/Manual:Hooks/AuthChangeFormFields
+	 * Moves login button to bottom of form.
+	 *
+	 * @since 2.0
+	 * @param array $requests AuthenticationRequests the fields are created from
+	 * @param array $fieldInfo union of AuthenticationRequest::getFieldInfo()
+	 * @param array &$formDescriptor The special key weight can be set to
+	 *        change the order of the fields.
+	 * @param string $action one of the AuthManager::ACTION_* constants.
+	 */
+	public function onAuthChangeFormFields( $requests, $fieldInfo, &$formDescriptor, $action ) {
+		$this->pluggableAuthService->moveLoginButton( $formDescriptor );
+	}
+
+	/**
+	 * Implements UserLogoutComplete hook.
+	 * See https://www.mediawiki.org/wiki/Manual:Hooks/UserLogoutComplete
+	 * Calls deauthenticate hook in authentication plugin.
+	 *
+	 * @since 2.0
+	 * @param User $user User after logout (won't have name, ID, etc.)
+	 * @param string &$inject_html Any HTML to inject after the logout message.
+	 * @param string $oldName The text of the username that just logged out.
+	 */
+	public function onUserLogoutComplete( $user, &$inject_html, $oldName ) {
+		$this->pluggableAuthService->deauthenticate( $oldName );
+	}
+
+	/**
+	 * Grab the page request early
+	 * See https://www.mediawiki.org/wiki/Manual:Hooks/BeforeInitialize
+	 * Redirects ASAP to login
+	 * @param Title $title being used for request
+	 * @param null $unused
+	 * @param OutputPage $output object
+	 * @param User $user current user
+	 * @param WebRequest $request why we're here
+	 * @param MediaWiki $mediaWiki object
+	 *
+	 * Note that $title has to be passed by ref so we can replace it.
+	 * @throws MWException
+	 */
+	public function onBeforeInitialize( $title, $unused, $output, $user, $request, $mediaWiki ) {
+		$this->pluggableAuthService->autoLogin( $title, $output, $user, $request );
+	}
+
+	/**
+	 * Implements PersonalUrls hook.
+	 * See https://www.mediawiki.org/wiki/Manual:Hooks/PersonalUrls
+	 * Removes logout link from skin if auto login is enabled and local login
+	 * is not enabled.
+	 *
+	 * @since 1.0
+	 *
+	 * @param array &$personal_urls urls sto modify
+	 * @param Title &$title current title
+	 * @param SkinTemplate $skin template for vars
+	 */
+	public function onPersonalUrls( &$personal_urls, &$title, $skin ): void {
+		$this->pluggableAuthService->removeLogoutLink( $personal_urls );
+	}
+
+	/**
+	 * Implements LocalUserCreated hook.
+	 * See https://www.mediawiki.org/wiki/Manual:Hooks/LocalUserCreated
+	 * Populate groups after the local user is created
+	 * Called immediately after a local user has been created and saved to the database.
+	 *
+	 * @since 5.5
+	 *
+	 * @param User $user current user
+	 * @param bool $autocreated whether the user was autocreated
+	 */
+	public function onLocalUserCreated( $user, $autocreated ) {
+		$this->pluggableAuthService->populateGroups( $user, $autocreated );
+	}
 
 	/**
 	 * Implements extension registration callback.
@@ -56,131 +174,5 @@ class PluggableAuthHooks {
 				}
 			}
 		}
-	}
-
-	/**
-	 * Implements TitleReadWhitelist hook.
-	 * See https://www.mediawiki.org/wiki/Manual:Hooks/TitleReadWhitelist
-	 * Adds PluggableAuth login special pages to whitelist.
-	 *
-	 * @since 2.0
-	 * @param Title $title being checked
-	 * @param User $user Current user
-	 * @param bool &$whitelisted whether this title is whitelisted
-	 *
-	 */
-	public static function onTitleReadWhitelist(
-		Title $title,
-		User $user,
-		bool &$whitelisted
-	) {
-		$pluggableAuthService = MediaWikiServices::getInstance()->get( 'PluggableAuthService' );
-		$pluggableAuthService->allowLoginPage( $title, $whitelisted );
-	}
-
-	/**
-	 * Implements AuthChangeFormFields hook.
-	 * See https://www.mediawiki.org/wiki/Manual:Hooks/AuthChangeFormFields
-	 * Moves login button to bottom of form.
-	 *
-	 * @since 2.0
-	 * @param array $requests AuthenticationRequests the fields are created from
-	 * @param array $fieldInfo union of AuthenticationRequest::getFieldInfo()
-	 * @param array &$formDescriptor The special key weight can be set to
-	 *        change the order of the fields.
-	 * @param string $action one of the AuthManager::ACTION_* constants.
-	 */
-	public static function onAuthChangeFormFields(
-		array $requests,
-		array $fieldInfo,
-		array &$formDescriptor,
-		string $action
-	) {
-		$pluggableAuthService = MediaWikiServices::getInstance()->get( 'PluggableAuthService' );
-		$pluggableAuthService->moveLoginButton( $formDescriptor );
-	}
-
-	/**
-	 * Implements UserLogoutComplete hook.
-	 * See https://www.mediawiki.org/wiki/Manual:Hooks/UserLogoutComplete
-	 * Calls deauthenticate hook in authentication plugin.
-	 *
-	 * @since 2.0
-	 * @param User $user User after logout (won't have name, ID, etc.)
-	 * @param string $inject_html Any HTML to inject after the logout message.
-	 * @param string $old_name The text of the username that just logged out.
-	 */
-	public static function deauthenticate(
-		User $user,
-		string $inject_html,
-		string $old_name
-	) {
-		$pluggableAuthService = MediaWikiServices::getInstance()->get( 'PluggableAuthService' );
-		$pluggableAuthService->deauthenticate( $old_name );
-	}
-
-	/**
-	 * Grab the page request early
-	 * See https://www.mediawiki.org/wiki/Manual:Hooks/BeforeInitialize
-	 * Redirects ASAP to login
-	 * @param Title &$title being used for request
-	 * @param null $article unused
-	 * @param OutputPage $out object
-	 * @param User $user current user
-	 * @param WebRequest $request why we're here
-	 * @param MediaWiki $mw object
-	 *
-	 * Note that $title has to be passed by ref so we can replace it.
-	 */
-	public static function doBeforeInitialize(
-		Title &$title,
-		$article,
-		OutputPage $out,
-		User $user,
-		WebRequest $request,
-		MediaWiki $mw
-	) {
-		$pluggableAuthService = MediaWikiServices::getInstance()->get( 'PluggableAuthService' );
-		$pluggableAuthService->autoLogin( $title, $out, $user, $request );
-	}
-
-	/**
-	 * Implements PersonalUrls hook.
-	 * See https://www.mediawiki.org/wiki/Manual:Hooks/PersonalUrls
-	 * Removes logout link from skin if auto login is enabled and local login
-	 * is not enabled.
-	 *
-	 * @since 1.0
-	 *
-	 * @param array &$personal_urls urls sto modify
-	 * @param Title|null $title current title
-	 * @param SkinTemplate|null $skin template for vars
-	 */
-	public static function modifyLoginURLs(
-		array &$personal_urls,
-		Title $title = null,
-		SkinTemplate $skin = null
-	) {
-		$pluggableAuthService = MediaWikiServices::getInstance()->get( 'PluggableAuthService' );
-		$pluggableAuthService->removeLogoutLink( $personal_urls );
-	}
-
-	/**
-	 * Implements LocalUserCreated hook.
-	 * See https://www.mediawiki.org/wiki/Manual:Hooks/LocalUserCreated
-	 * Populate groups after the local user is created
-	 * Called immediately after a local user has been created and saved to the database.
-	 *
-	 * @since 5.5
-	 *
-	 * @param User $user current user
-	 * @param bool $autocreated whether the user was autocreated
-	 */
-	public static function onLocalUserCreated(
-		User $user,
-		bool $autocreated
-	) {
-		$pluggableAuthService = MediaWikiServices::getInstance()->get( 'PluggableAuthService' );
-		$pluggableAuthService->populateGroups( $user, $autocreated );
 	}
 }
